8a04781e3784deeb99b119281c156cf4a1d5a8bd
Santi Collazo <santi@collazo.eu>
2026-07-10 13:58:19 +0200

    Create AM databases using inline mysql

--- .archivematica-init/archivematica-databases.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE MCP CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-CREATE DATABASE SS CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-GRANT ALL ON MCP.* TO 'archivematica'@'%';
-GRANT ALL ON SS.* TO 'archivematica'@'%';
